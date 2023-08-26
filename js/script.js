7269ced3fd1b207334f8a6d6d7d6b8e4cde39bb4
Gabrielle Tirta <tirtagabrielle@gmail.com>
2023-08-26 19:45:35 +1000

    removed demo function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
 console.log("hi");
-
-function codingBoss() {
-    // this is a alert when codingBoss is called
-    // alert("hey you!");
-    document.querySelector('h1').style.color= "hotpink";
-}
-codingBoss();
