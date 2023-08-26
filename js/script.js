db61a4fb645c2a28297457d8348f9f95b91d1c04
Gabrielle Tirta <tirtagabrielle@gmail.com>
2023-08-26 18:55:56 +1000

    first initial push live

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("hi");
+
+function codingBoss() {
+    // this is a alert when codingBoss is called
+    // alert("hey you!");
+    document.querySelector('h1').style.color= "hotpink";
+}
+codingBoss();
